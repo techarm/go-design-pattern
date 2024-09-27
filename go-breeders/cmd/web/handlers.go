db86270c51355aba7d00dfb5279d49a0471097b5
techarm <techarmdev@gmail.com>
2024-09-27 22:11:55 +0900

    using builder pattern to create pets

--- go-breeders/cmd/web/handlers.go
+++ go-breeders/cmd/web/handlers.go
@@ -57,3 +57,50 @@ func (app *application) GetAllDogBreedsJSON(w http.ResponseWriter, r *http.Reque
 	}
 	_ = t.WriteJSON(w, http.StatusOK, dogBreeds)
 }
+
+func (app *application) CreateDogWithBuilder(w http.ResponseWriter, r *http.Request) {
+	var t toolbox.Tools
+
+	// create a dog using the builder pattern
+	p, err := pets.NewPetBuilder().
+		SetSpecies("dog").
+		SetBreed("mixed breed").
+		SetWeight(15).
+		SetMinWeight(10).
+		SetMaxWeight(30).
+		SetDescription("A mixed breed of unknow origin.").
+		SetColor("Block and White").
+		SetAge(3).
+		SetAgeEstimated(true).
+		Build()
+
+	if err != nil {
+		_ = t.ErrorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+	_ = t.WriteJSON(w, http.StatusOK, p)
+}
+
+func (app *application) CreateCatWithBuilder(w http.ResponseWriter, r *http.Request) {
+	var t toolbox.Tools
+
+	// create a dog using the builder pattern
+	p, err := pets.NewPetBuilder().
+		SetSpecies("cat").
+		SetBreed("felis slivestris catus").
+		SetWeight(4).
+		SetMinWeight(3).
+		SetMaxWeight(8).
+		SetDescription("A beautiful house cat.").
+		SetColor("Calico").
+		SetAge(1).
+		SetGeographicOrigin("Canada").
+		SetAgeEstimated(true).
+		Build()
+
+	if err != nil {
+		_ = t.ErrorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+	_ = t.WriteJSON(w, http.StatusOK, p)
+}
